55183912acddbbea49762cd831da6a3b96554500
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-20 08:30:54 +0100

    :sparkles: Fetch and render characters from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import Character from'./CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [characters, setCharacters] = useState([])
+  const [url, setUrl] = useState('https://rickandmortyapi.com/api/character/')
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    
+    axios.get(url)
+    .then(response => {
+      console.log(response.data);
+      setCharacters(response.data.results)
+    })
+    .catch(error => {
+      console.log(error);
+      
+    })
+  }, [url]);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        characters.map(character => <Character character={character}/>)
+      }
     </section>
   );
 }
