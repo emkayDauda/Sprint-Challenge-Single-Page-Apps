48065aaa1a1cb6bfef49508e067abddbe32038f8
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-20 08:27:14 +0100

    :sparkles: Fill out character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,19 @@
 import React from "react";
+import {Name, StyledCharacter} from './StyledComponents'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard({character}) {
+
+    return(
+        <StyledCharacter>
+            <img src={character.image} alt=""/>
+            <Name>{character.name}</Name>
+            <div>
+                <p>Species: {character.species}</p>
+                <p>Status: {character.status}</p>
+            </div>
+            <p>Gender: {character.gender}</p>
+            <p>Number of episodes: {character.episode.length}</p>
+        </StyledCharacter>
+    )
 }
