f875b9fb40a01577223f6320979444cec7547aa9
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-20 08:23:48 +0100

    :sparkles: Import and render all components in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import Welcome from './components/WelcomePage';
+import CharaList from './components/CharacterList'
+import LocationList from './components/LocationsList'
+import SearchForm from './components/SearchForm'
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Welcome />
+      <CharaList />
+      <LocationList />
+      <SearchForm />
     </main>
   );
 }
